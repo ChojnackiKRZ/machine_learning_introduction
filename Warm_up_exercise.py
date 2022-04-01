5edfac69eb397f06b355867a9a83d8044e71388b
Krzysztof Chojnacki <krzysztof.michal.chojnacki@gmail.com>
2022-04-01 21:53:29 +0200

    Update Warm_up_exercise.py

--- Warm_up_exercise.py
+++ Warm_up_exercise.py
@@ -4,7 +4,7 @@ Created on Sun Mar 13 17:47:58 2022
 
 @author: krzys
 """
-    
+
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
@@ -13,36 +13,38 @@ import os
 from scipy.stats.stats import pearsonr
 import numpy as np
 from typing import List
-  
+
+
+path = r"C:\Users\krzys\Desktop\data science\IV semestr\machine_learning\machine_learning_introduction"
+os.chdir(path)
+
+x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
+y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
+y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
+y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
+x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
+y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
+
+list_y = [y1, y2, y3, y4]
+list_x = [x, x4]
+
 
 def main():
-    
-    path = r"C:\Users\krzys\Desktop\data science\IV semestr\machine_learning\machine_learning_introduction"
-    os.chdir(path)
-    
-    x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
-    y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
-    y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
-    y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
-    x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
-    y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
-    
-    list_y = [y1, y2, y3, y4]
-    list_x = [x, x4]
-    
+
     datasetscsv = {}
     datasets = {}
-    
+
     def stat_data(
-        x_axis: List[float] or List[int], y_axis: List[float] or List[int]
+        x_axis: List[float] or List[int], y_axis: List[float] or List[int], save=False
     ) -> dict:
         """
         Function takes arguments for x_axis and y_axis as list of ints or floats,
         calculates: 1) std, 2) var, 3) mean for y, 4) corr for x and y and saves
         it as csv in path in separate folder.
+        Argument save: if True dataset will be saved as csv.
         Also returns dict with the same values as saved to csv.
         """
-    
+
         if type(x_axis) != list or type(y_axis) != list:
             raise TypeError("arguments must be lists of ints or floats")
         try:
@@ -51,7 +53,7 @@ def main():
             pass
         df = pd.DataFrame()
         df_main = pd.DataFrame()
-    
+
         n = 0
         for x in x_axis:
             for y in y_axis:
@@ -65,7 +67,8 @@ def main():
                 }
                 df = pd.DataFrame(data=datasetscsv[n], index=[n])
                 df_main = pd.concat([df_main, df])
-                df_main.to_csv("csv\data_stats.csv", index=False)
+                if save == True:
+                    df_main.to_csv("csv\data_stats.csv", index=False)
                 n += 1
         n = 0
         for i in datasetscsv.items():
@@ -75,38 +78,53 @@ def main():
             datasetscsv[n]["y"] = list(map(float, datasetscsv[n]["y"].split(",")))
             n = n + 1
         return datasetscsv
-    
-        return datasetscsv
-    
-    datasets = stat_data(list_x, list_y)
-    
-    def scatter_plots(datasets: dict) -> plt:
+
+
+    def scatter_plots(
+        x_axis: List[float] or List[int], y_axis: List[float] or List[int]
+    ) -> plt:
         """
-        This function takes as argument dict created with function stat_data.
-        Draws scatter plot with stats on it and saves it in path location.
+        Function takes arguments for x_axis and y_axis as list of ints or floats,
+        calculates: 1) std, 2) var, 3) mean for y, 4) corr and puts it on plots.
         """
-    
+
+        datasets = stat_data(x_axis, y_axis)
+
         try:
             os.mkdir(os.path.join(path, "plots"))
         except FileExistsError:
             pass
         fig, axs = plt.subplots(
-            2,
-            2,
+            len (x_axis),
+            len (y_axis),
             sharex=True,
             sharey=True,
-            figsize=(8, 6),
+            figsize=((len (x_axis) * len (y_axis))*2, len (x_axis) * len (y_axis)),
             gridspec_kw={"wspace": 0.08, "hspace": 0.20},
         )
+        
+        # for i in range (0, len (x_axis) * len (y_axis)):
+        # x_axis_number = len (x_axis)
+        # y_axis_number = len (y_axis)
+        # for x in x_axis_number:
+        #     for y in y_axis_number:
         axs[0, 0].scatter(datasets[0]["x"], datasets[0]["y"])
         axs[0, 0].set_title("Axis [0, 0]")
         axs[0, 1].scatter(datasets[1]["x"], datasets[1]["y"])
-        axs[0, 1].set_title("Axis [0, 1]")
-        axs[1, 0].scatter(datasets[2]["x"], datasets[2]["y"])
+        axs[0, 1].set_title("Axis [0, 1]")            
+        axs[0, 2].scatter(datasets[2]["x"], datasets[2]["y"])
+        axs[0, 2].set_title("Axis [0, 2]")
+        axs[0, 3].scatter(datasets[3]["x"], datasets[3]["y"])
+        axs[0, 3].set_title("Axis [0, 3]")
+        axs[1, 0].scatter(datasets[4]["x"], datasets[4]["y"])
         axs[1, 0].set_title("Axis [1, 0]")
-        axs[1, 1].scatter(datasets[3]["x"], datasets[3]["y"])
+        axs[1, 1].scatter(datasets[5]["x"], datasets[5]["y"])
         axs[1, 1].set_title("Axis [1, 1]")
-    
+        axs[1, 2].scatter(datasets[6]["x"], datasets[6]["y"])
+        axs[1, 2].set_title("Axis [1, 2]")
+        axs[1, 3].scatter(datasets[7]["x"], datasets[7]["y"])
+        axs[1, 3].set_title("Axis [1, 3]")
+
         n = 0
         for ax in axs.flat:
             ax.label_outer()
@@ -127,9 +145,10 @@ def main():
             )
             n += 1
         fig.savefig("plots\scatter.jpg")
-    
-    scatter_plots(datasets)
-    
-if __name__ == '__main__':
+
+    scatter_plots(list_x, list_y)
+
+
+if __name__ == "__main__":
     main()
     
